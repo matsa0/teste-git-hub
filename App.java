995d31ea970bc33bc33608ed6cee2e535e01b524
Matheus Azevedo <matheus.azevedo@aluno.ufop.edu.br>
2023-09-11 18:37:52 -0300

    Commit 2 para testar mudanças

--- App.java
+++ App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) {
         System.out.println("Teste commit 1 java");
+        System.out.println("Commit 2 para testar mudança");
     }
 }
