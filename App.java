5ab8bad960e797507bc8b7b5b947bd914c301070
Matheus Azevedo <matheus.azevedo@aluno.ufop.edu.br>
2023-09-12 18:46:40 -0300

    Testando desfazer commit sem perder alterações

--- App.java
+++ App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Teste commit 1 java");
         System.out.println("Commit 2 para testar mudança");
+        System.out.println("Teste para desfazer commit sem perder alterações");
     }
 }
